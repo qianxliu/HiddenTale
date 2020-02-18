1eb87c26bfc9621f854d414a12331a2bfae7df4e
neteraxe <neteraxe@outlook.com>
2020-02-18 11:54:42 +0800

    Change layout

--- app/src/main/java/com/example/myapplication/MapInitActivity.java
+++ app/src/main/java/com/example/myapplication/MapInitActivity.java
@@ -27,7 +27,7 @@ public class MapInitActivity extends Activity {
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         //aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW) ;
+        aMap.showIndoorMap(true);
 
 
     }
